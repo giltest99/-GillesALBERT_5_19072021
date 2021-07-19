9dcc85749427783ef4c02fb3a019644b35fd2ad5
gilles <sellig.blater@free.com>
2021-07-19 21:13:46 +0200

    Add fetch products function

---
+++ js/getProductList.js
@@ -0,0 +1,30 @@
+function getProducts(){
+    let url = 'http://localhost:3000/api/teddies';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        for(product of data){
+
+            let card = `
+            
+            <div class="col-12 col-sm-6 col-lg-4" id="${product._id}">                
+                <div class="card shadow-sm my-3">
+                    <img src="${product.imageUrl}" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.name}</h5>
+                        <p class="card-text">${product.description}</p>
+                        <h5 class="card-title">${(product.price/100).toFixed(2).replace('.',',')}€</h5>                      
+                    </div>
+                </div>
+            </div>
+
+            `;
+
+            document.querySelector('#productsSection').innerHTML += card;
+        }
+    });
+    
+}
+getProducts();

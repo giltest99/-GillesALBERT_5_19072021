718269c149baacf8a7307fe2b3250e2ac6fca76a
gilles <sellig.blater@free.com>
2021-07-19 23:07:00 +0200

    Misc fixes

--- js/getProductList.js
+++ js/getProductList.js
@@ -8,23 +8,25 @@ function getProducts(){
         for(product of data){
 
             let card = `
-            
-            <div class="col-12 col-sm-6 col-lg-4" id="${product._id}">                
-                <div class="card shadow-sm my-3">
-                    <img src="${product.imageUrl}" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text">${product.description}</p>
-                        <h5 class="card-title">${(product.price/100).toFixed(2).replace('.',',')}€</h5>                      
-                    </div>
+                <div class="col-12 col-sm-6 col-lg-4" id="${product._id}"> 
+                    <a href="http://localhost:3000/api/teddies/${product._id}">               
+                        <div class="card shadow-sm my-3">
+                            <img src="${product.imageUrl}" alt="Référence ${product.name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.name}</h5>
+                                <p class="card-text">${product.description}</p>
+                                <h5 class="card-title">${(product.price/100).toFixed(2).replace('.',',')}€</h5>                      
+                            </div>
+                        </div>
+                    </a>
                 </div>
-            </div>
-
             `;
 
             document.querySelector('#productsSection').innerHTML += card;
+            
         }
-    });
+    })
+    
     
 }
 getProducts();

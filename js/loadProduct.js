de31769502c5ba79c7a398a33c6d708eed9743ca
gilles <sellig.blater@free.com>
2021-08-05 16:22:20 +0200

    Add customer form on panier.html

--- js/loadProduct.js
+++ js/loadProduct.js
@@ -1,45 +1,51 @@
-function loadProduct(){
-    let params = new URLSearchParams(document.location.search.substring(1));
-      let id = params.get("id");
-      console.log(id)
-  
-      let url = `http://localhost:3000/api/teddies/${id}`;
-      fetch(url)
-      .then(res => res.json())
-      .then(data => {
+function loadProduct() {
+  const params = new URLSearchParams(document.location.search);
+  //let params = new URLSearchParams(document.location.search.substring(1));
+  let id = params.get("id");
+  console.log(id);
+
+  let url = `http://localhost:3000/api/teddies/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
       console.log(data);
-      let app = document.querySelector('#app');
-      let cardTemplate = document.querySelector('#card-template');
+      const app = document.querySelector("#app");
+      const cardTemplate = document.querySelector("#card-template");
       //let selectCard = document.querySelector('#card-select');
       //console.log(selectCard)
-      let card = cardTemplate.content.cloneNode(true);
-      app.appendChild(card)
+      const card = cardTemplate.content.cloneNode(true);
+      app.appendChild(card);
       const colors = data.colors;
-      console.log(colors , colors.length);
-      for(color of colors){
-        let opt = document.createElement('OPTION');
+      console.log(colors, colors.length);
+      for (color of colors) {
+        let opt = document.createElement("OPTION");
         opt.value = color;
         opt.innerHTML = color;
-        document.querySelector('#app select').appendChild(opt);    
+        document.querySelector("#app select").appendChild(opt);
       }
-      document.querySelector('#image-pres').src = data.imageUrl;
+      document.querySelector("#image-pres").src = data.imageUrl;
       //document.querySelector('#image-pres').style.height = '100%';
-      document.querySelector('#image-pres').style.objectFit = 'cover';
+
+      // Line to remove and to replace by style.css
+      //document.querySelector("#image-pres").style.objectFit = "cover"; 
       //document.querySelector('#nom').textContent = data.name;
       //document.querySelector('#description').textContent = data.description;
       //document.querySelector('#prix').textContent = Number(data.price / 100).toFixed(2).replace('.',',') + '€';
-      
-      populate('#image-pres', data.imageUrl);
-      populate('#nom', data.name);
-      populate('#description', data.description);
-      populate('#prix', Number(data.price / 100).toFixed(2).replace('.',',') + '€');
-    })  
-    
-  }
-  
-  loadProduct()
-  
-  function populate(selector,data){
-    document.querySelector(selector).textContent = data;
-  }
-    
+
+      populate("#image-pres", data.imageUrl);
+      populate("#nom", data.name);
+      populate("#description", data.description);
+      populate(
+        "#prix",
+        Number(data.price / 100)
+          .toFixed(2)
+          .replace(".", ",") + "€"
+      );
+    });
+}
+
+loadProduct();
+
+function populate(selector, data) {
+  document.querySelector(selector).textContent = data;
+}

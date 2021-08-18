8cc16db86177ac9a1b9b79d0c666aa306e1a205c
gilles <sellig.blater@free.com>
2021-08-18 21:56:12 +0200

    Misc fixes

--- js/getBasket.js
+++ js/getBasket.js
@@ -40,14 +40,12 @@ function storeCustomerInfos() {
     const address = fieldValidation("address");
     const city = fieldValidation("city");
     const email = fieldValidation("email");
-
     if (firstName && lastName && address && city && email) {
         obj.firstName = firstName;
         obj.lastName = lastName;
         obj.address = address;
         obj.city = city;
         obj.email = email;
-
         localStorage.setItem("ORINOCO_CUSTOMER_INFOS", JSON.stringify(obj));
         console.log("Infos saved !");
         alert("Informations sauvegardées !");
